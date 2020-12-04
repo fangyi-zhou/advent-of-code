d27ab82266ce20ed0c60bda0a0084b685debccb8
Fangyi Zhou <me@fangyi.io>
2020-12-04 10:42:02 +0000

    Day 4 part 2

--- lib/day4.ml
+++ lib/day4.ml
@@ -43,28 +43,79 @@ let process_passport (acc, curr) line =
     let curr = List.fold ~init:curr ~f entries in
     (acc, curr)
 
+let validate_passport byr iyr eyr hgt hcl ecl pid cid passport =
+  byr passport.byr && iyr passport.iyr && eyr passport.eyr
+  && hgt passport.hgt && hcl passport.hcl && ecl passport.ecl
+  && pid passport.pid && cid passport.cid
+
 let part1 data =
-  let f passport =
-    Option.is_some passport.byr
-    && Option.is_some passport.iyr
-    && Option.is_some passport.eyr
-    && Option.is_some passport.hgt
-    && Option.is_some passport.hcl
-    && Option.is_some passport.ecl
-    && Option.is_some passport.pid
+  let f =
+    validate_passport Option.is_some Option.is_some Option.is_some
+      Option.is_some Option.is_some Option.is_some Option.is_some (fun _ ->
+        true)
   in
   List.count ~f data
 
-let day4 inputs =
+let part2 data =
+  let num_range_suffix mini maxi suffix str_opt =
+    try
+      let str = Option.value_exn str_opt in
+      let str = String.chop_suffix_exn str ~suffix in
+      let num = Int.of_string str in
+      num >= mini && num <= maxi
+    with _ -> false
+  in
+  let num_range mini maxi str = num_range_suffix mini maxi "" str in
+  let hair_color str_opt =
+    match str_opt with
+    | Some str ->
+        let is_hex ch =
+          Char.((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f'))
+        in
+        String.length str = 7
+        && Char.equal str.[0] '#'
+        && List.for_all [1; 2; 3; 4; 5; 6] ~f:(fun idx -> is_hex str.[idx])
+    | None -> false
+  in
+  let eye_color = function
+    | Some ("amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth") -> true
+    | _ -> false
+  in
+  let passport_id = function
+    | Some str ->
+        String.length str = 9
+        && String.for_all ~f:(fun ch -> Char.(ch >= '0' && ch <= '9')) str
+    | None -> false
+  in
+  let f =
+    validate_passport (num_range 1920 2002) (num_range 2010 2020)
+      (num_range 2020 2030)
+      (fun s ->
+        num_range_suffix 150 193 "cm" s || num_range_suffix 59 76 "in" s)
+      hair_color eye_color passport_id
+      (fun _ -> true)
+  in
+  List.count ~f data
+
+let day4 ?only_part1 ?only_part2 inputs =
   let lines = String.split ~on:'\n' inputs in
   let data, last =
     List.fold ~init:([], new_passport) ~f:process_passport lines
   in
   let data = List.rev (last :: data) in
-  let answer1 = part1 data in
-  print_endline (Int.to_string answer1)
+  let () =
+    if Option.is_none only_part2 then
+      let answer1 = part1 data in
+      print_endline (Int.to_string answer1)
+  in
+  let () =
+    if Option.is_none only_part1 then
+      let answer2 = part2 data in
+      print_endline (Int.to_string answer2)
+  in
+  ()
 
-let example =
+let example_1 =
   "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n\
    byr:1937 iyr:2017 cid:147 hgt:183cm\n\n\
    iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\n\
@@ -76,4 +127,37 @@ let example =
    hcl:#cfa07d eyr:2025 pid:166559648\n\
    iyr:2011 ecl:brn hgt:59in"
 
-let%expect_test _ = day4 example ; [%expect {|2|}]
+let example_2 =
+  "eyr:1972 cid:100\n\
+   hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\n\n\
+   iyr:2019\n\
+   hcl:#602927 eyr:1967 hgt:170cm\n\
+   ecl:grn pid:012533040 byr:1946\n\n\
+   hcl:dab227 iyr:2012\n\
+   ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\n\n\
+   hgt:59cm ecl:zzz\n\
+   eyr:2038 hcl:74454a iyr:2023\n\
+   pid:3556412378 byr:2007"
+
+let example_3 =
+  "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\n\
+   hcl:#623a2f\n\n\
+   eyr:2029 ecl:blu cid:129 byr:1989\n\
+   iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n\n\
+   hcl:#888785\n\
+   hgt:164cm byr:2001 iyr:2015 cid:88\n\
+   pid:545766238 ecl:hzl\n\
+   eyr:2022\n\n\
+   iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+
+let%expect_test _ =
+  day4 ~only_part1:true example_1 ;
+  [%expect {|2|}]
+
+let%expect_test _ =
+  day4 ~only_part2:true example_2 ;
+  [%expect {|0|}]
+
+let%expect_test _ =
+  day4 ~only_part2:true example_3 ;
+  [%expect {|4|}]
