20f9c37228d37127d49ddca94626de553d9ff928
Fangyi Zhou <me@fangyi.io>
2020-12-09 17:08:11 +0000

    Day 9 Part 2

--- lib/day9.ml
+++ lib/day9.ml
@@ -10,7 +10,7 @@ module M = struct
 
   let capacity = 25
 
-  let part1 nums =
+  let part1_with_capacity ~capacity nums =
     let queue = Queue.create ~capacity () in
     let ans =
       List.fold_until ~init:()
@@ -29,9 +29,73 @@ module M = struct
         ~finish:(fun _ -> assert false)
         nums
     in
+    ans
+
+  let part1 nums =
+    let ans = part1_with_capacity ~capacity nums in
     print_endline (Int.to_string ans)
 
-  let part2 _ = ()
+  let part2_with_capacity ~capacity nums =
+    let subseq_sum = part1_with_capacity ~capacity nums in
+    let find_ans queue =
+      Option.value_exn (Queue.min_elt queue ~compare)
+      + Option.value_exn (Queue.max_elt queue ~compare)
+    in
+    let queue = Queue.create () in
+    List.fold_until ~init:0
+      ~f:(fun acc elem ->
+        let rec aux acc elem : _ Continue_or_stop.t =
+          let acc' = acc + elem in
+          if acc' = subseq_sum then (
+            Queue.enqueue queue elem ;
+            Stop (find_ans queue) )
+          else if acc' < subseq_sum then (
+            Queue.enqueue queue elem ; Continue acc' )
+          else
+            let first = Queue.dequeue_exn queue in
+            aux (acc - first) elem
+        in
+        aux acc elem)
+      ~finish:(fun _ -> assert false)
+      nums
+
+  let part2 nums =
+    let ans = part2_with_capacity ~capacity nums in
+    print_endline (Int.to_string ans)
 end
 
+include M
 include Day.Make (M)
+
+let example =
+  "35\n\
+   20\n\
+   15\n\
+   25\n\
+   47\n\
+   40\n\
+   62\n\
+   55\n\
+   65\n\
+   95\n\
+   102\n\
+   117\n\
+   150\n\
+   182\n\
+   127\n\
+   219\n\
+   299\n\
+   277\n\
+   309\n\
+   576"
+
+let%expect_test _ =
+  let nums = parse example in
+  let ans1 = part1_with_capacity ~capacity:5 nums in
+  print_endline (Int.to_string ans1) ;
+  let ans2 = part2_with_capacity ~capacity:5 nums in
+  print_endline (Int.to_string ans2) ;
+  [%expect {|
+    127
+    62
+              |}]
