674e412e0f77978294a73b3ed768a40df4c496aa
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2020-12-12 20:31:39 +0000

    Day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -13,83 +13,145 @@ fn b2i(input: bool) -> i32 {
     }
 }
 
-fn run(input: &mut [i32], mut input_val: VecDeque<i32>) -> i32 {
-    let mut pc: usize = 0;
-    let mut output: Option<i32> = None;
-    loop {
-        let instr = input[pc];
-        let opcode = instr % 100;
-        match opcode {
-            1 | 2 | 7 | 8 => {
-                let operand1 = if (instr / 100 % 10) == 0 {
-                    input[input[pc + 1] as usize]
-                } else {
-                    input[pc + 1]
-                };
-                let operand2 = if (instr / 1000 % 10) == 0 {
-                    input[input[pc + 2] as usize]
-                } else {
-                    input[pc + 2]
-                };
-                let dest = input[pc + 3] as usize;
-                let result = match opcode {
-                    1 => operand1 + operand2,
-                    2 => operand1 * operand2,
-                    7 => b2i(operand1 < operand2),
-                    8 => b2i(operand1 == operand2),
-                    _ => unreachable!(),
-                };
-                input[dest] = result;
-                pc += 4;
-            }
-            3 => {
-                let dest = input[pc + 1] as usize;
-                input[dest] = input_val.pop_front().unwrap();
-                pc += 2;
-            }
-            4 => {
-                let output_src = input[pc + 1] as usize;
-                output = Some(input[output_src]);
-                pc += 2;
-            }
-            5 | 6 => {
-                let operand1 = if (instr / 100 % 10) == 0 {
-                    input[input[pc + 1] as usize]
-                } else {
-                    input[pc + 1]
-                };
-                let operand2 = if (instr / 1000 % 10) == 0 {
-                    input[input[pc + 2] as usize]
-                } else {
-                    input[pc + 2]
-                };
-                pc = if opcode == 5 && operand1 != 0 || opcode == 6 && operand1 == 0 {
-                    operand2 as usize
-                } else {
-                    pc + 3
+struct IntMachine {
+    memory: Vec<i32>,
+    inputs: VecDeque<i32>,
+    outputs: VecDeque<i32>,
+    pc: usize,
+    finished: bool,
+}
+
+impl IntMachine {
+    fn new(memory: &mut [i32]) -> IntMachine {
+        IntMachine {
+            memory: Vec::from(memory),
+            inputs: VecDeque::new(),
+            outputs: VecDeque::new(),
+            pc: 0,
+            finished: false,
+        }
+    }
+
+    fn input(&mut self, val: i32) {
+        self.inputs.push_back(val)
+    }
+
+    fn output(&mut self) -> Option<i32> {
+        self.outputs.pop_front()
+    }
+
+    fn run(&mut self) {
+        if self.finished {
+            return;
+        }
+        loop {
+            let instr = self.memory[self.pc];
+            let opcode = instr % 100;
+            match opcode {
+                1 | 2 | 7 | 8 => {
+                    let operand1 = if (instr / 100 % 10) == 0 {
+                        self.memory[self.memory[self.pc + 1] as usize]
+                    } else {
+                        self.memory[self.pc + 1]
+                    };
+                    let operand2 = if (instr / 1000 % 10) == 0 {
+                        self.memory[self.memory[self.pc + 2] as usize]
+                    } else {
+                        self.memory[self.pc + 2]
+                    };
+                    let dest = self.memory[self.pc + 3] as usize;
+                    let result = match opcode {
+                        1 => operand1 + operand2,
+                        2 => operand1 * operand2,
+                        7 => b2i(operand1 < operand2),
+                        8 => b2i(operand1 == operand2),
+                        _ => unreachable!(),
+                    };
+                    self.memory[dest] = result;
+                    self.pc += 4;
                 }
-            }
-            99 => {
-                break;
-            }
-            _ => {
-                unreachable!();
+                3 => {
+                    let dest = self.memory[self.pc + 1] as usize;
+                    match self.inputs.pop_front() {
+                        Some(input) => {
+                            self.memory[dest] = input;
+                            self.pc += 2
+                        }
+                        None => {
+                            break;
+                        }
+                    }
+                }
+                4 => {
+                    let output_src = self.memory[self.pc + 1] as usize;
+                    self.outputs.push_back(self.memory[output_src]);
+                    self.pc += 2;
+                }
+                5 | 6 => {
+                    let operand1 = if (instr / 100 % 10) == 0 {
+                        self.memory[self.memory[self.pc + 1] as usize]
+                    } else {
+                        self.memory[self.pc + 1]
+                    };
+                    let operand2 = if (instr / 1000 % 10) == 0 {
+                        self.memory[self.memory[self.pc + 2] as usize]
+                    } else {
+                        self.memory[self.pc + 2]
+                    };
+                    self.pc = if opcode == 5 && operand1 != 0 || opcode == 6 && operand1 == 0 {
+                        operand2 as usize
+                    } else {
+                        self.pc + 3
+                    }
+                }
+                99 => {
+                    self.finished = true;
+                    break;
+                }
+                _ => {
+                    unreachable!();
+                }
             }
         }
     }
-    output.unwrap()
 }
 
 fn run_with_config(program: &[i32], config: &[i32]) -> i32 {
     let mut input_val = 0;
     for i in 0..5 {
         let mut program_copy = program.to_owned();
-        let mut input = VecDeque::new();
-        input.push_back(config[i]);
-        input.push_back(input_val);
-        input_val = run(program_copy.as_mut(), input);
+        let mut machine = IntMachine::new(program_copy.as_mut());
+        machine.input(config[i]);
+        machine.input(input_val);
+        machine.run();
+        input_val = machine.output().unwrap();
     }
     input_val
+}
+
+fn run_with_config_forever(program: &[i32], config: &[i32]) -> i32 {
+    let mut machines = Vec::new();
+    for i in 0..5 {
+        let mut program_copy = program.to_owned().clone();
+        let mut machine = IntMachine::new(program_copy.as_mut());
+        machine.input(config[i]);
+        machines.push(machine);
+    }
+    machines[0].input(0);
+    loop {
+        let mut all_finished = true;
+        for i in 0..5 {
+            machines[i].run();
+            if let Some(output) = machines[i].output() {
+                machines[(i + 1) % 5].input(output);
+            }
+            all_finished = all_finished && machines[i].finished;
+        }
+        if all_finished {
+            break;
+        }
+    }
+    machines[0].inputs.pop_front().unwrap()
 }
 
 fn generate_config(start: i32, end: i32) -> Vec<Vec<i32>> {
@@ -125,5 +187,13 @@ pub fn solve_part1(input: &[i32]) -> i32 {
     let input = input.to_owned();
     generate_config(0, 5).iter().fold(0, |acc, config| {
         i32::max(acc, run_with_config(&input, config))
+    })
+}
+
+#[aoc(day7, part2)]
+pub fn solve_part2(input: &[i32]) -> i32 {
+    let input = input.to_owned();
+    generate_config(5, 10).iter().fold(0, |acc, config| {
+        i32::max(acc, run_with_config_forever(&input, config))
     })
 }
