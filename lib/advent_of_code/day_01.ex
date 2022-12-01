c1dadb9f0e375d69b767c9564f0ccf630c04971b
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2022-12-01 12:22:18 +0000

    d1p2

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,25 +1,29 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
-    input = AdventOfCode.Input.get!(1)
-    lines = String.split(input, "\n")
-    iterate(lines, 0, 0)
-  end
-
-  defp iterate(items, curr, maxi) do
+  defp iterate(items, curr, accum) do
     case items do
       [] ->
-        maxi
+        accum
 
       ["" | rest] ->
-        newmaxi = max(curr, maxi)
-        iterate(rest, 0, newmaxi)
+        [_ | new_accum] = Enum.sort([curr | accum], :asc)
+        iterate(rest, 0, new_accum)
 
       [num_s | rest] ->
         {num, _} = Integer.parse(num_s)
-        iterate(rest, curr + num, maxi)
+        iterate(rest, curr + num, accum)
     end
   end
 
+  def part1(_args) do
+    input = AdventOfCode.Input.get!(1)
+    lines = String.split(input, "\n")
+    [ret] = iterate(lines, 0, [0])
+    ret
+  end
+
   def part2(_args) do
+    input = AdventOfCode.Input.get!(1)
+    lines = String.split(input, "\n")
+    Enum.reduce(iterate(lines, 0, [0, 0, 0]), 0, fn x, y -> x + y end)
   end
 end
