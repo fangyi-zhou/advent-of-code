dc1abccade9421dd4b309894b3143fdcb9bf59ed
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2022-12-01 11:51:25 +0000

    d1p1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,5 +1,23 @@
 defmodule AdventOfCode.Day01 do
   def part1(_args) do
+    input = AdventOfCode.Input.get!(1)
+    lines = String.split(input, "\n")
+    iterate(lines, 0, 0)
+  end
+
+  defp iterate(items, curr, maxi) do
+    case items do
+      [] ->
+        maxi
+
+      ["" | rest] ->
+        newmaxi = max(curr, maxi)
+        iterate(rest, 0, newmaxi)
+
+      [num_s | rest] ->
+        {num, _} = Integer.parse(num_s)
+        iterate(rest, curr + num, maxi)
+    end
   end
 
   def part2(_args) do
