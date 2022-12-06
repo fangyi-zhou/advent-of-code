b6541ca6db7ab11d0612f90c75df5cf5051fd3ca
Fangyi Zhou <me@fangyi.io>
2022-12-06 11:39:05 +0000

    d6p2

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,18 +1,18 @@
 defmodule AdventOfCode.Day06 do
-  defp all_unique?(stuffs) do
-    MapSet.size(MapSet.new(stuffs)) == 4
+  defp all_unique?(stuffs, threshold) do
+    MapSet.size(MapSet.new(stuffs)) == threshold
   end
 
-  defp find_unique_start([hd | rest], idx, acc) do
-    if idx < 4 do
-      find_unique_start(rest, idx + 1, acc ++ [hd])
+  defp find_unique_start([hd | rest], idx, acc, threshold) do
+    if idx < threshold do
+      find_unique_start(rest, idx + 1, acc ++ [hd], threshold)
     else
-      if all_unique?([hd | acc]) do
+      if all_unique?([hd | acc], threshold) do
         idx
       else
         case acc do
           [_ | acc_rest] ->
-            find_unique_start(rest, idx + 1, acc_rest ++ [hd])
+            find_unique_start(rest, idx + 1, acc_rest ++ [hd], threshold)
         end
       end
     end
@@ -22,9 +22,13 @@ defmodule AdventOfCode.Day06 do
     input = AdventOfCode.Input.get!(6)
     input = String.trim(input)
     chars = String.to_charlist(input)
-    find_unique_start(chars, 1, [])
+    find_unique_start(chars, 1, [], 4)
   end
 
   def part2(_args) do
+    input = AdventOfCode.Input.get!(6)
+    input = String.trim(input)
+    chars = String.to_charlist(input)
+    find_unique_start(chars, 1, [], 14)
   end
 end
