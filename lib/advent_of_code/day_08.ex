f2b225b09c35fdb98c0e0a7c3387e68804fcace8
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2022-12-08 12:39:07 +0000

    d8 (part 2 is still wrong)

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,210 @@
 defmodule AdventOfCode.Day08 do
+  defp line_to_map(chars, x, y, acc) do
+    case chars do
+      [] ->
+        {acc, y}
+
+      [hd | tl] ->
+        acc = Map.put(acc, {x, y}, hd - ?0)
+        line_to_map(tl, x, y + 1, acc)
+    end
+  end
+
+  defp to_map(lines, x, y, acc) do
+    case lines do
+      [] ->
+        {acc, x, y}
+
+      [hd | tl] ->
+        chars = String.to_charlist(hd)
+        {acc, y} = line_to_map(chars, x, 0, acc)
+        to_map(tl, x + 1, y, acc)
+    end
+  end
+
+  defp horizontal_lookups_h(map, x, y, i, j, left, right) do
+    cond do
+      i == x ->
+        {left, right}
+
+      j == y ->
+        horizontal_lookups_h(map, x, y, i + 1, 0, left, right)
+
+      j == 0 ->
+        curr_l = Map.get(map, {i, j})
+        left = Map.put(left, {i, j}, curr_l)
+        curr_r = Map.get(map, {i, y - j - 1})
+        right = Map.put(right, {i, y - j - 1}, curr_r)
+        horizontal_lookups_h(map, x, y, i, j + 1, left, right)
+
+      true ->
+        curr_l = Map.get(map, {i, j})
+        max_l = Map.get(left, {i, j - 1})
+        left = Map.put(left, {i, j}, max(curr_l, max_l))
+        curr_r = Map.get(map, {i, y - j - 1})
+        max_r = Map.get(right, {i, y - j})
+        right = Map.put(right, {i, y - j - 1}, max(curr_r, max_r))
+        horizontal_lookups_h(map, x, y, i, j + 1, left, right)
+    end
+  end
+
+  defp vertical_lookups_h(map, x, y, i, j, top, bottom) do
+    cond do
+      j == y ->
+        {top, bottom}
+
+      i == x ->
+        vertical_lookups_h(map, x, y, 0, j + 1, top, bottom)
+
+      i == 0 ->
+        curr_t = Map.get(map, {i, j})
+        top = Map.put(top, {i, j}, curr_t)
+        curr_b = Map.get(map, {x - i - 1, j})
+        bottom = Map.put(bottom, {x - i - 1, j}, curr_b)
+        vertical_lookups_h(map, x, y, i + 1, j, top, bottom)
+
+      true ->
+        curr_t = Map.get(map, {i, j})
+        max_t = Map.get(top, {i - 1, j})
+        top = Map.put(top, {i, j}, max(curr_t, max_t))
+        curr_b = Map.get(map, {x - i - 1, j})
+        max_b = Map.get(bottom, {x - i, j})
+        bottom = Map.put(bottom, {x - i - 1, j}, max(curr_b, max_b))
+        vertical_lookups_h(map, x, y, i + 1, j, top, bottom)
+    end
+  end
+
+  defp make_height_lookups(map, x, y) do
+    {left, right} = horizontal_lookups_h(map, x, y, 0, 0, Map.new(), Map.new())
+    {top, bottom} = vertical_lookups_h(map, x, y, 0, 0, Map.new(), Map.new())
+    {left, right, top, bottom}
+  end
+
+  defp find_visible(map, height_lookups, i, j, x, y, acc) do
+    cond do
+      i == x ->
+        acc
+
+      j == y ->
+        find_visible(map, height_lookups, i + 1, 0, x, y, acc)
+
+      i == 0 or j == 0 or i == x - 1 or j == y - 1 ->
+        find_visible(map, height_lookups, i, j + 1, x, y, acc + 1)
+
+      true ->
+        curr = Map.get(map, {i, j})
+        {left, right, top, bottom} = height_lookups
+        left = Map.get(left, {i, j - 1})
+        right = Map.get(right, {i, j + 1})
+        top = Map.get(top, {i - 1, j})
+        bottom = Map.get(bottom, {i + 1, j})
+        # IO.puts("#{i} #{j} #{curr} #{left} #{right} #{top} #{bottom}")
+
+        acc =
+          if curr > left or curr > right or curr > top or curr > bottom, do: acc + 1, else: acc
+
+        find_visible(map, height_lookups, i, j + 1, x, y, acc)
+    end
+  end
+
+  defp horizontal_lookups_d(map, x, y, i, j, left, right) do
+    cond do
+      i == x ->
+        {left, right}
+
+      j == y ->
+        horizontal_lookups_d(map, x, y, i + 1, 0, left, right)
+
+      j == 0 ->
+        left = Map.put(left, {i, j}, 0)
+        right = Map.put(right, {i, y - j - 1}, 0)
+        horizontal_lookups_d(map, x, y, i, j + 1, left, right)
+
+      true ->
+        dist_l = Map.get(left, {i, j - 1})
+        val_l = if Map.get(map, {i, j}) > Map.get(map, {i, j - 1}), do: dist_l + 1, else: 1
+        left = Map.put(left, {i, j}, val_l)
+        dist_r = Map.get(right, {i, y - j})
+
+        val_r =
+          if Map.get(map, {i, y - j - 1}) > Map.get(map, {i, y - j}), do: dist_r + 1, else: 1
+
+        right = Map.put(right, {i, y - j - 1}, val_r)
+        horizontal_lookups_d(map, x, y, i, j + 1, left, right)
+    end
+  end
+
+  defp vertical_lookups_d(map, x, y, i, j, top, bottom) do
+    cond do
+      j == y ->
+        {top, bottom}
+
+      i == x ->
+        vertical_lookups_d(map, x, y, 0, j + 1, top, bottom)
+
+      i == 0 ->
+        top = Map.put(top, {i, j}, 0)
+        bottom = Map.put(bottom, {x - i - 1, j}, 0)
+        vertical_lookups_d(map, x, y, i + 1, j, top, bottom)
+
+      true ->
+        dist_t = Map.get(top, {i - 1, j})
+        val_t = if Map.get(map, {i, j}) > Map.get(map, {i - 1, j}), do: dist_t + 1, else: 1
+        top = Map.put(top, {i, j}, val_t)
+        dist_b = Map.get(bottom, {x - i, j})
+
+        val_b =
+          if Map.get(map, {x - i - 1, j}) > Map.get(map, {x - i, j}), do: dist_b + 1, else: 1
+
+        bottom = Map.put(bottom, {x - i - 1, j}, val_b)
+        vertical_lookups_d(map, x, y, i + 1, j, top, bottom)
+    end
+  end
+
+  defp find_best(map, distance_lookups, i, j, x, y, acc) do
+    cond do
+      i == x ->
+        acc
+
+      j == y ->
+        find_best(map, distance_lookups, i + 1, 0, x, y, acc)
+
+      i == 0 or j == 0 or i == x - 1 or j == y - 1 ->
+        find_best(map, distance_lookups, i, j + 1, x, y, acc)
+
+      true ->
+        {left, right, top, bottom} = distance_lookups
+        left = Map.get(left, {i, j})
+        right = Map.get(right, {i, j})
+        top = Map.get(top, {i, j})
+        bottom = Map.get(bottom, {i, j})
+        IO.puts("#{i} #{j} L#{left} R#{right} T#{top} B#{bottom} #{left * right * top * bottom}")
+
+        find_best(map, distance_lookups, i, j + 1, x, y, max(left * right * top * bottom, acc))
+    end
+  end
+
+  defp make_distance_lookups(map, x, y) do
+    {left, right} = horizontal_lookups_d(map, x, y, 0, 0, Map.new(), Map.new())
+    {top, bottom} = vertical_lookups_d(map, x, y, 0, 0, Map.new(), Map.new())
+    {left, right, top, bottom}
+  end
+
   def part1(_args) do
+    input = AdventOfCode.Input.get!(8)
+    input = String.trim(input)
+    lines = String.split(input, "\n")
+    {map, x, y} = to_map(lines, 0, 0, Map.new())
+    height_lookups = make_height_lookups(map, x, y)
+    find_visible(map, height_lookups, 0, 0, x, y, 0)
   end
 
   def part2(_args) do
+    input = AdventOfCode.Input.get!(8)
+    input = String.trim(input)
+    lines = String.split(input, "\n")
+    {map, x, y} = to_map(lines, 0, 0, Map.new())
+    distance_lookups = make_distance_lookups(map, x, y)
+    find_best(map, distance_lookups, 0, 0, x, y, 0)
   end
 end
