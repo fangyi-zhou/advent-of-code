82b5d55023c295ab6bcbae6ff82587c63aa1f1c7
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2022-12-09 16:09:22 +0000

    Refactor d9

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,45 +1,36 @@
 defmodule AdventOfCode.Day09 do
-  defp move_step(dir) do
-    case dir do
-      "R" -> {1, 0}
-      "U" -> {0, 1}
-      "L" -> {-1, 0}
-      "D" -> {0, -1}
-    end
+  defp move_step("R"), do: {1, 0}
+  defp move_step("U"), do: {0, 1}
+  defp move_step("L"), do: {-1, 0}
+  defp move_step("D"), do: {0, -1}
+
+  defp update_tail({x_head, y_head}, {x_tail, y_tail})
+       when abs(x_head - x_tail) <= 1 and abs(y_head - y_tail) <= 1 do
+    # no need to move
+    {x_tail, y_tail}
+  end
+
+  defp update_tail({x_head, y_head}, {x_tail, y_tail})
+       when x_head != x_tail and y_head != y_tail and abs(x_head - x_tail) == 1 and
+              abs(y_head - y_tail) == 2 do
+    {x_head, div(y_head + y_tail, 2)}
+  end
+
+  defp update_tail({x_head, y_head}, {x_tail, y_tail})
+       when x_head != x_tail and y_head != y_tail and abs(y_head - y_tail) == 1 and
+              abs(x_head - x_tail) == 2 do
+    {div(x_head + x_tail, 2), y_head}
   end
 
   defp update_tail({x_head, y_head}, {x_tail, y_tail}) do
-    cond do
-      abs(x_head - x_tail) <= 1 and abs(y_head - y_tail) <= 1 ->
-        # no need to move
-        {x_tail, y_tail}
-
-      x_head != x_tail and y_head != y_tail ->
-        cond do
-          abs(x_head - x_tail) == 1 and abs(y_head - y_tail) == 2 ->
-            {x_head, div(y_head + y_tail, 2)}
-
-          abs(y_head - y_tail) == 1 and abs(x_head - x_tail) == 2 ->
-            {div(x_head + x_tail, 2), y_head}
-
-          true ->
-            {div(x_head + x_tail, 2), div(y_head + y_tail, 2)}
-        end
-
-      true ->
-        {div(x_head + x_tail, 2), div(y_head + y_tail, 2)}
-    end
+    {div(x_head + x_tail, 2), div(y_head + y_tail, 2)}
   end
 
-  defp update_tails(prev, tails) do
-    case tails do
-      [] ->
-        []
+  defp update_tails(_, []), do: []
 
-      [tail | tails] ->
-        new_tail = update_tail(prev, tail)
-        [new_tail | update_tails(new_tail, tails)]
-    end
+  defp update_tails(prev, [tail | tails]) do
+    new_tail = update_tail(prev, tail)
+    [new_tail | update_tails(new_tail, tails)]
   end
 
   defp move(visited, knots, dir, step) do
@@ -58,40 +49,26 @@ defmodule AdventOfCode.Day09 do
     end
   end
 
-  defp go(lines, visited, knots) do
-    case lines do
-      [] ->
-        MapSet.size(visited)
+  defp go([], visited, _), do: MapSet.size(visited)
 
-      [line | rest] ->
-        [dir, steps] = String.split(line, " ")
-        {steps, _} = Integer.parse(steps)
-        {visited, knots} = move(visited, knots, dir, steps)
-        go(rest, visited, knots)
-    end
+  defp go([line | rest], visited, knots) do
+    [dir, steps] = String.split(line, " ")
+    {steps, _} = Integer.parse(steps)
+    {visited, knots} = move(visited, knots, dir, steps)
+    go(rest, visited, knots)
   end
 
   def part1(_args) do
     input = AdventOfCode.Input.get!(9)
     lines = String.split(input, "\n", trim: true)
-    go(lines, MapSet.new(), [{0, 0}, {0, 0}])
+    knots = List.duplicate({0, 0}, 2)
+    go(lines, MapSet.new(), knots)
   end
 
   def part2(_args) do
     input = AdventOfCode.Input.get!(9)
     lines = String.split(input, "\n", trim: true)
-
-    go(lines, MapSet.new(), [
-      {0, 0},
-      {0, 0},
-      {0, 0},
-      {0, 0},
-      {0, 0},
-      {0, 0},
-      {0, 0},
-      {0, 0},
-      {0, 0},
-      {0, 0}
-    ])
+    knots = List.duplicate({0, 0}, 10)
+    go(lines, MapSet.new(), knots)
   end
 end
