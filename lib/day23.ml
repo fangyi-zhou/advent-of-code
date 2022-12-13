f5f428102ccd79ad841349c5b99c097ae851a1d8
Fangyi Zhou <me@fangyi.io>
2022-12-13 23:08:55 +0000

    Improve 2020/d23

--- lib/day23.ml
+++ lib/day23.ml
@@ -57,6 +57,93 @@ module Ring = struct
   let to_list = function hd, curr, tl -> [curr] @ tl @ List.rev hd
 end
 
+module Ring_Map = struct
+  (* Each map entry corresponds to next number and prev number; integer
+     represents the focus *)
+
+  type t = (int * int) Map.M(Int).t * int
+
+  let focus elem (entries, _) = (entries, elem)
+
+  let remove_right n (entries, curr) =
+    let rec aux acc iter entries =
+      if iter = n then (List.rev acc, (entries, curr))
+      else
+        let _, to_remove = Map.find_exn entries curr in
+        let _, to_fix = Map.find_exn entries to_remove in
+        let entries = Map.remove entries to_remove in
+        let entries =
+          Map.update entries curr ~f:(function
+            | Some (prev, _) -> (prev, to_fix)
+            | None -> assert false )
+        in
+        let entries =
+          Map.update entries curr ~f:(function
+            | Some (_, next) -> (curr, next)
+            | None -> assert false )
+        in
+        aux (to_remove :: acc) (iter + 1) entries
+    in
+    aux [] 0 entries
+
+  let of_list xs =
+    match xs with
+    | [] -> assert false
+    | hd :: tl ->
+        let interleaved = List.zip_exn xs (tl @ [hd]) in
+        ( List.fold
+            ~init:(Map.empty (module Int))
+            ~f:(fun acc (prev, curr) ->
+              let acc =
+                Map.update acc curr ~f:(function
+                  | None -> (prev, 0)
+                  | Some (_, next) -> (prev, next) )
+              in
+              let acc =
+                Map.update acc prev ~f:(function
+                  | None -> (0, curr)
+                  | Some (prevprev, _) -> (prevprev, curr) )
+              in
+              acc )
+            interleaved
+        , hd )
+
+  let current (_, curr) = curr
+
+  let shift_right (entries, curr) =
+    let _, next = Map.find_exn entries curr in
+    (entries, next)
+
+  let insert_right elems (entries, curr) =
+    let rev_elems = List.rev elems in
+    ( List.fold ~init:entries
+        ~f:(fun entries new_elem ->
+          let _, to_fix = Map.find_exn entries curr in
+          let entries =
+            Map.add_exn entries ~key:new_elem ~data:(curr, to_fix)
+          in
+          let entries =
+            Map.update entries curr ~f:(function
+              | Some (prev, _) -> (prev, new_elem)
+              | None -> assert false )
+          in
+          let entries =
+            Map.update entries to_fix ~f:(function
+              | Some (_, next) -> (new_elem, next)
+              | None -> assert false )
+          in
+          entries )
+        rev_elems
+    , curr )
+
+  let to_list (entries, focus) =
+    let rec aux acc curr =
+      let _, next = Map.find_exn entries curr in
+      if next = focus then List.rev acc else aux (next :: acc) next
+    in
+    aux [focus] focus
+end
+
 module M = struct
   type t = int list
 
@@ -76,41 +163,53 @@ module M = struct
     | curr when curr <> e1 && curr <> e2 && curr <> e3 -> curr
     | curr -> find_dest (curr - 1) e1 e2 e3 ~limit
 
+
+  let count = ref 0
+
+  let incr_count () =
+    count := !count + 1
+
+  let reset_count () = 
+    count := 0
+
   let move ~limit ring =
-    let hd = Ring.current ring in
-    match Ring.remove_right 3 ring with
+    if !count % 100000 = 0 then
+      print_endline_int !count ;
+    incr_count () ;
+    let hd = Ring_Map.current ring in
+    match Ring_Map.remove_right 3 ring with
     | [e1; e2; e3], ring ->
         let dest = find_dest (hd - 1) e1 e2 e3 ~limit in
-        let ring = Ring.focus dest ring in
-        let ring = Ring.insert_right [e1; e2; e3] ring in
-        let ring = Ring.focus hd ring in
-        let ring = Ring.shift_right ring in
+        let ring = Ring_Map.focus dest ring in
+        let ring = Ring_Map.insert_right [e1; e2; e3] ring in
+        let ring = Ring_Map.focus hd ring in
+        let ring = Ring_Map.shift_right ring in
         ring
     | _ -> assert false
 
   let move_with_print ~limit input =
     let output = move ~limit input in
-    print_endline (string_of_list @@ Ring.to_list output) ;
+    print_endline (string_of_list @@ Ring_Map.to_list output) ;
     output
 
   let part1 init =
-    let init = Ring.of_list init in
+    reset_count ();
+    let init = Ring_Map.of_list init in
     let final = Fn.apply_n_times (move ~limit:9) ~n:100 init in
-    let final = Ring.focus 1 final in
-    let final = Ring.to_list final in
+    let final = Ring_Map.focus 1 final in
+    let final = Ring_Map.to_list final in
     let ans = string_of_list (List.tl_exn final) in
     print_endline ans
 
-  let _part2 init =
+  let part2 init =
+    reset_count ();
     let init = init @ List.range ~stop:`inclusive 10 1000000 in
-    let init = Ring.of_list init in
+    let init = Ring_Map.of_list init in
     let final = Fn.apply_n_times (move ~limit:1000000) ~n:10000000 init in
-    let final = Ring.focus 1 final in
-    let two_to_the_right, _ = Ring.remove_right 2 final in
+    let final = Ring_Map.focus 1 final in
+    let two_to_the_right, _ = Ring_Map.remove_right 2 final in
     let ans = List.product two_to_the_right in
     print_endline_int ans
-
-  let part2 _ = ()
 end
 
 include Day.Make (M)
