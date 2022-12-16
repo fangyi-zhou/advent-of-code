db6937f79360b8c22d4d2a1e7e85e7d73d83bb9a
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2022-12-16 16:19:30 +0000

    d16: just search

--- 2022/lib/advent_of_code/day_16.ex
+++ 2022/lib/advent_of_code/day_16.ex
@@ -45,37 +45,50 @@ Valve JJ has flow rate=21; tunnel leads to valve II"
   end
 
   defp go(_, _, valves_to_open, _, _, acc) when map_size(valves_to_open) == 0, do: acc
+  defp go(_, _, _, _, time, acc) when time <= 0, do: acc
 
   defp go(flows, connections, valves_to_open, curr, time, acc) do
-    # IO.puts("At #{curr}, time #{time}")
+    # IO.puts("At #{curr}, time #{time} acc #{acc}")
+    # IO.inspect(valves_to_open)
 
-    {ret, pressure} =
-      Enum.reduce(valves_to_open, {"", 0}, fn {candidate, flow}, {old_candidate, old_pressure} ->
-        dist = find_dist(connections, curr, candidate)
-        pressure = max(0, (time - dist - 1) * flow)
-        # IO.puts("#{candidate} at #{dist} away, pressure #{pressure}")
+    # {ret, pressure} =
+    #   Enum.reduce(valves_to_open, {"", 0}, fn {candidate, flow}, {old_candidate, old_pressure} ->
+    #     dist = find_dist(connections, curr, candidate)
+    #     pressure = max(0, (time - dist - 1) * flow)
+    #     # IO.puts("#{candidate} at #{dist} away, pressure #{pressure}")
 
-        if pressure > old_pressure,
-          do: {{candidate, dist}, pressure},
-          else: {old_candidate, old_pressure}
-      end)
+    #     if pressure > old_pressure,
+    #       do: {{candidate, dist}, pressure},
+    #       else: {old_candidate, old_pressure}
+    #   end)
 
-    if pressure == 0 do
-      acc
-    else
-      {candidate, dist} = ret
+    # if pressure == 0 do
+    #   acc
+    # else
+    #   {candidate, dist} = ret
+    #   valves_to_open = Map.delete(valves_to_open, candidate)
+    #   go(flows, connections, valves_to_open, candidate, time - dist - 1, acc + pressure)
+    # end
+    Enum.reduce(valves_to_open, 0, fn {candidate, flow}, prev_best ->
+      dist = find_dist(connections, curr, candidate)
+      pressure = max(0, (time - dist - 1) * flow)
+      # IO.puts("#{candidate} at #{dist} away, pressure #{pressure}")
       valves_to_open = Map.delete(valves_to_open, candidate)
-      go(flows, connections, valves_to_open, candidate, time - dist - 1, acc + pressure)
-    end
+
+      max(
+        prev_best,
+        go(flows, connections, valves_to_open, candidate, time - dist - 1, acc + pressure)
+      )
+    end)
   end
 
   def part1(_args) do
-    input = test
+    # input = test
     input = AdventOfCode.Input.get!(16)
     lines = String.split(input, "\n", trim: true)
     {flows, connections} = Enum.reduce(lines, {Map.new(), Map.new()}, &parse_line/2)
     valves_to_open = Map.filter(flows, fn {_, flow} -> flow > 0 end)
-    IO.inspect(valves_to_open)
+    # IO.inspect(valves_to_open)
     go(flows, connections, valves_to_open, "AA", 30, 0)
   end
 
