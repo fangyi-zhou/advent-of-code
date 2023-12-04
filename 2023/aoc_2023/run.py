291907e9f9decfc36cbeba646683c86248ba374f
Fangyi Zhou <me@fangyi.io>
2023-12-04 21:56:48 +0000

    2023 day 1 part 2

--- 2023/aoc_2023/run.py
+++ 2023/aoc_2023/run.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     year = 2023
     solution = importlib.import_module("solutions.day" + day)
     input_file = f"inputs/{day}.txt"
-    if not os.path.exists(input_file):
+    if not os.path.exists(input_file) and len(sys.argv) <= 2:
         with open(".session") as f:
             session = f.read().strip()
         r = requests.get(
@@ -24,10 +24,11 @@ if __name__ == "__main__":
         with open(input_file, "w") as f:
             f.write(r.text)
     else:
+        input_file = sys.argv[2] if len(sys.argv) > 2 else input_file
         with open(input_file) as f:
             input_data = f.read().strip()
     parsed = solution.parse(input_data)
     if hasattr(solution, "part1"):
-        print(solution.part1(parsed))
+       print(solution.part1(parsed))
     if hasattr(solution, "part2"):
         print(solution.part2(parsed))
--- 2023/aoc_2023/solutions/day1.py
+++ 2023/aoc_2023/solutions/day1.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Generator
 
 
 def parse(input: str) -> List[str]:
@@ -11,3 +11,40 @@ def part1(lines: List[str]) -> int:
         return digits[0] * 10 + digits[-1]
 
     return sum(map(part1_solve, lines))
+
+
+DIGITS_MAP = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+def part2(lines: List[str]) -> int:
+    def gen_digits(line: str) -> Generator[int, None, None]:
+        while line:
+            matched = False
+            if line[0] >= "0" and line[0] <= "9":
+                digit = ord(line[0]) - ord("0")
+                matched = True
+                yield digit
+            if not matched:
+                for word, digit in DIGITS_MAP.items():
+                    if line.startswith(word):
+                        matched = True
+                        yield digit
+                        break
+            line = line[1:]
+
+    def part2_solve(line: str) -> int:
+        digits = [*gen_digits(line)]
+        print(digits)
+        return digits[0] * 10 + digits[-1]
+
+    return sum(map(part2_solve, lines))
