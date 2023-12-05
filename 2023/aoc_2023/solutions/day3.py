86d07a9445559e555ad0d028d03ebde5cf59baa0
Fangyi Zhou <me@fangyi.io>
2023-12-05 22:27:16 +0000

    2023 day 3 part 2

--- 2023/aoc_2023/solutions/day3.py
+++ 2023/aoc_2023/solutions/day3.py
@@ -1,4 +1,5 @@
-from typing import List
+from collections import defaultdict
+from typing import List, Tuple, Optional
 import re
 
 
@@ -31,3 +32,36 @@ def part1(lines: List[str]) -> int:
             if not search_for_parts(i, j - len(digit_buf), len(digit_buf)):
                 results.append(int(digit_buf))
     return sum(results)
+
+
+def part2(lines: List[str]) -> int:
+    def search_for_gear(x: int, y_start: int, length: int) -> Tuple[int, int]:
+        for i in range(x - 1, x + 2):
+            for j in range(y_start - 1, y_start + length + 1):
+                if i >= 0 and i < len(lines) and j >= 0 and j < len(lines[0]):
+                    if lines[i][j] == "*":
+                        return (i, j)
+        return None
+
+    gear_map: Dict[Tuple[int, int], List[int]] = defaultdict(list)
+    for i, line in enumerate(lines):
+        digit_buf = ""
+        for j, ch in enumerate(line):
+            if ch >= "0" and ch <= "9":
+                digit_buf += ch
+            else:
+                if digit_buf:
+                    gear = search_for_gear(i, j - len(digit_buf), len(digit_buf))
+                    if gear is not None:
+                        gear_map[gear].append(int(digit_buf))
+                    digit_buf = ""
+        j += 1
+        if digit_buf:
+            gear = search_for_gear(i, j - len(digit_buf), len(digit_buf))
+            if gear is not None:
+                gear_map[gear].append(int(digit_buf))
+    result = 0
+    for gear_numbers in gear_map.values():
+        if len(gear_numbers) == 2:
+            result += gear_numbers[0] * gear_numbers[1]
+    return result
