78e80e550a9f04fa659c3a3c6dbed39125d0f83f
Fangyi Zhou <me@fangyi.io>
2023-12-08 22:08:03 +0000

    2023 day 8 part 2

--- 2023/aoc_2023/solutions/day8.py
+++ 2023/aoc_2023/solutions/day8.py
@@ -1,4 +1,5 @@
 from typing import Dict, Tuple
+import math
 from itertools import cycle
 
 
@@ -9,9 +10,10 @@ def parse(lines: str) -> Tuple[str, Dict[str, Tuple[str, str]]]:
         return node_name.strip(), node_left.strip()[1:], node_right.strip()[:-1]
 
     sequence, _, *nodes = lines.split("\n")
-    nodes_map: Dict[str, Tuple[str, str]] = {}
-    for node_name, node_left, node_right in map(parse_node, nodes):
-        nodes_map[node_name] = (node_left, node_right)
+    nodes_map: Dict[str, Tuple[str, str]] = {
+        node_name: (node_left, node_right)
+        for node_name, node_left, node_right in map(parse_node, nodes)
+    }
     return sequence, nodes_map
 
 
@@ -26,21 +28,23 @@ def _step(nodes: Dict[str, Tuple[str, str]], current: str, direction: str) -> st
 def part1(network: Tuple[str, Dict[str, Tuple[str, str]]]) -> int:
     sequence, nodes = network
     current = "AAA"
-    steps = 0
-    for next_dir in cycle(sequence):
-        steps += 1
+    for steps, next_dir in enumerate(cycle(sequence)):
         current = _step(nodes, current, next_dir)
         if current == "ZZZ":
-            return steps
+            return steps + 1
     raise RuntimeError("Unreachable")
 
 
-# def part2(network: Tuple[str, Dict[str, Tuple[str, str]]]):
-#     sequence, nodes = network
-#     currents = [*filter(lambda n: n[-1] == "A", nodes)]
-#     steps = 0
-#     for next_dir in cycle(sequence):
-#         steps += 1
-#         currents = [*map(lambda current: _step(nodes, current, next_dir), currents)]
-#         if all(n[-1] == "Z" for n in currents):
-#             return steps
+def part2(network: Tuple[str, Dict[str, Tuple[str, str]]]) -> int:
+    sequence, nodes = network
+    currents = set(filter(lambda n: n[-1] == "A", nodes))
+    steps_done = []
+    for steps, next_dir in enumerate(cycle(sequence)):
+        currents = set(map(lambda current: _step(nodes, current, next_dir), currents))
+        done = set(filter(lambda n: n[-1] == "Z", currents))
+        if done:
+            steps_done.append(steps + 1)
+            currents = currents - done
+        if not currents:
+            break
+    return math.lcm(*steps_done)
