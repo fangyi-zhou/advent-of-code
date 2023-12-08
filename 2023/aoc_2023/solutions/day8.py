94ef343b6cea3a9ccc4234d4d9d19c49a7cdc7f4
Fangyi Zhou <me@fangyi.io>
2023-12-08 17:56:33 +0000

    WIP 2023 day 8 part 2

--- 2023/aoc_2023/solutions/day8.py
+++ 2023/aoc_2023/solutions/day8.py
@@ -15,15 +15,32 @@ def parse(lines: str) -> Tuple[str, Dict[str, Tuple[str, str]]]:
     return sequence, nodes_map
 
 
-def part1(network: Tuple[str, Dict[str, Tuple[str, str]]]):
+def _step(nodes: Dict[str, Tuple[str, str]], current: str, direction: str) -> str:
+    if direction == "L":
+        return nodes[current][0]
+    elif direction == "R":
+        return nodes[current][1]
+    raise RuntimeError("Invalid direction " + direction)
+
+
+def part1(network: Tuple[str, Dict[str, Tuple[str, str]]]) -> int:
     sequence, nodes = network
     current = "AAA"
     steps = 0
     for next_dir in cycle(sequence):
         steps += 1
-        if next_dir == "L":
-            current = nodes[current][0]
-        elif next_dir == "R":
-            current = nodes[current][1]
+        current = _step(nodes, current, next_dir)
         if current == "ZZZ":
             return steps
+    raise RuntimeError("Unreachable")
+
+
+# def part2(network: Tuple[str, Dict[str, Tuple[str, str]]]):
+#     sequence, nodes = network
+#     currents = [*filter(lambda n: n[-1] == "A", nodes)]
+#     steps = 0
+#     for next_dir in cycle(sequence):
+#         steps += 1
+#         currents = [*map(lambda current: _step(nodes, current, next_dir), currents)]
+#         if all(n[-1] == "Z" for n in currents):
+#             return steps
