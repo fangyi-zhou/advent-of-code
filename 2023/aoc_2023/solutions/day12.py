5fb64cbb1651b99611b5fbf3261c638d32e1cecf
Fangyi Zhou <me@fangyi.io>
2023-12-12 21:33:15 +0000

    2023 day 12 part 1 slightly more optimised

--- 2023/aoc_2023/solutions/day12.py
+++ 2023/aoc_2023/solutions/day12.py
@@ -29,15 +29,77 @@ def _summarise(pattern: str, replacement: List[str]) -> List[int]:
     return counts
 
 
+DEBUG = False
+
+
+def _fill_blanks(pattern: str, expectation: List[int], memory: str = "") -> int:
+    if DEBUG:
+        print(memory, pattern, expectation)
+    if not pattern and all(exp == 0 for exp in expectation):
+        if DEBUG:
+            print("ok")
+        return 1
+    if pattern and not expectation:
+        if all(ch != "#" for ch in pattern):
+            if DEBUG:
+                print("ok")
+            return 1
+        else:
+            return 0
+    if not pattern and expectation:
+        return 0
+    if expectation[0] == 0 and pattern[0] == ".":
+        return _fill_blanks(pattern[1:], expectation[1:], memory + ".")
+    if expectation[0] == 0 and pattern[0] == "#":
+        return 0
+    if expectation[0] > 0 and pattern[0] == "#":
+        return _fill_blanks(
+            pattern[1:], [expectation[0] - 1] + expectation[1:], memory + "#"
+        )
+    if pattern[0] == ".":
+        if memory and memory[-1] == "#" and expectation[0] > 0:
+            return 0
+        return _fill_blanks(pattern[1:], expectation, memory + ".")
+    if pattern[0] == "?":
+        result = 0
+        if expectation[0] > 0:
+            result += _fill_blanks(
+                pattern[1:], [expectation[0] - 1] + expectation[1:], memory + "#"
+            )
+            if memory and memory[-1] == "#":
+                return result
+        if expectation[0] == 0:
+            result += _fill_blanks(pattern[1:], expectation[1:], memory + ".")
+        else:
+            result += _fill_blanks(pattern[1:], expectation, memory + ".")
+        return result
+    return 0
+
+
 def part1(rows: List[Tuple[str, List[int]]]) -> int:
     arrangements = 0
     for pattern, summary in rows:
-        unknowns = pattern.count("?")
-        known_damages = pattern.count("#")
-        unknown_damages = sum(summary) - known_damages
-        unknown_operationals = unknowns - unknown_damages
-        replacements = "." * unknown_operationals + "#" * unknown_damages
-        for replacement in multiset_permutations(replacements):
-            if _summarise(pattern, replacement) == summary:
-                arrangements += 1
+        result = _fill_blanks(pattern, summary)
+        arrangements += result
+        # old_result = 0
+        # unknowns = pattern.count("?")
+        # known_damages = pattern.count("#")
+        # unknown_damages = sum(summary) - known_damages
+        # unknown_operationals = unknowns - unknown_damages
+        # replacements = "." * unknown_operationals + "#" * unknown_damages
+        # for replacement in multiset_permutations(replacements):
+        #     if _summarise(pattern, replacement) == summary:
+        #         old_result += 1
+        # if old_result != new_result:
+        #     print(pattern, summary, old_result, new_result)
     return arrangements
+
+
+# def part2(rows: List[Tuple[str, List[int]]]) -> int:
+#     arrangements = 0
+#     for pattern, summary in rows:
+#         pattern = "?".join([pattern] * 5)
+#         summary = sum([summary] * 5, [])
+#         result = _fill_blanks(pattern, summary)
+#         arrangements += result
+#     return arrangements
