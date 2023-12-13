4d849362db0c306275fb37c548a1090241439e27
Fangyi Zhou <me@fangyi.io>
2023-12-13 20:36:12 +0000

    2023 day 13 part 2

--- 2023/aoc_2023/solutions/day13.py
+++ 2023/aoc_2023/solutions/day13.py
@@ -7,24 +7,35 @@ def parse(inputs: str) -> List[List[str]]:
     return [list(g) for k, g in groupby(lines, lambda k: k != "") if k]
 
 
-def part1(inputs: List[List[str]]) -> int:
-    def find_mirror(pattern: List[str]) -> int:
-        for j in range(1, len(pattern[0])):
-            cols = min(j, len(pattern[0]) - j)
-            if all(
-                pattern[i][j - j_ - 1] == pattern[i][j + j_]
+def find_mirror_with_smudge(pattern: List[str], smudge: int) -> int:
+    for j in range(1, len(pattern[0])):
+        cols = min(j, len(pattern[0]) - j)
+        if (
+            sum(
+                pattern[i][j - j_ - 1] != pattern[i][j + j_]
                 for i in range(len(pattern))
                 for j_ in range(cols)
-            ):
-                return j
-        for i in range(1, len(pattern)):
-            rows = min(i, len(pattern) - i)
-            if all(
-                pattern[i - i_ - 1][j] == pattern[i + i_][j]
+            )
+            == smudge
+        ):
+            return j
+    for i in range(1, len(pattern)):
+        rows = min(i, len(pattern) - i)
+        if (
+            sum(
+                pattern[i - i_ - 1][j] != pattern[i + i_][j]
                 for i_ in range(rows)
                 for j in range(len(pattern[0]))
-            ):
-                return 100 * i
-        raise RuntimeError("unreachable")
+            )
+            == smudge
+        ):
+            return 100 * i
+    raise RuntimeError("unreachable")
 
-    return sum(map(find_mirror, inputs))
+
+def part1(inputs: List[List[str]]) -> int:
+    return sum(map(lambda p: find_mirror_with_smudge(p, 0), inputs))
+
+
+def part2(inputs: List[List[str]]) -> int:
+    return sum(map(lambda p: find_mirror_with_smudge(p, 1), inputs))
