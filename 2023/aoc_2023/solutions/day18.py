4b4856efec2ad2240eced2cd405551553b6f0175
Fangyi Zhou <me@fangyi.io>
2023-12-18 23:50:10 +0000

    2023 day 18 part 1 optimised

--- 2023/aoc_2023/solutions/day18.py
+++ 2023/aoc_2023/solutions/day18.py
@@ -23,10 +23,20 @@ def part1(plan: List[Tuple[str, int, str]]) -> int:
     curr_x, curr_y = 0, 0
     min_x, max_x = 0, 0
     min_y, max_y = 0, 0
+    area = 0
     walls: Set[Tuple[int, int]] = set([(curr_x, curr_y)])
     for line in plan:
         direction, num, _ = line
         dx, dy = DIRECTION[direction]
+        if direction == "U":
+            base = -curr_x
+        elif direction == "D":
+            base = curr_x
+        elif direction == "L":
+            base = curr_y
+        else:
+            base = -curr_y
+        area += num * (base + 1)
         for i in range(num):
             curr_x, curr_y = curr_x + dx, curr_y + dy
             min_x = min(min_x, curr_x)
@@ -34,35 +44,11 @@ def part1(plan: List[Tuple[str, int, str]]) -> int:
             max_x = max(max_x, curr_x)
             max_y = max(max_y, curr_y)
             walls.add((curr_x, curr_y))
-    exterior: Set[Tuple[int, int]] = set()
-
-    def fill(i: int, j: int) -> None:
-        task_list: List[Tuple[int, int]] = []
-        task_list.append((i, j))
-        while task_list:
-            i, j = task_list.pop()
-            if (i, j) in walls or (i, j) in exterior:
-                continue
-            exterior.add((i, j))
-            if i > min_x:
-                task_list.append((i - 1, j))
-            if i < max_x:
-                task_list.append((i + 1, j))
-            if j > min_y:
-                task_list.append((i, j - 1))
-            if j < max_y:
-                task_list.append((i, j + 1))
-
-    for i in range(min_x, max_x + 1):
-        fill(i, min_y)
-        fill(i, max_y)
-    for j in range(min_y, max_y + 1):
-        fill(min_x, j)
-        fill(max_x, j)
     # for y in range(min_y, max_y + 1):
     #     for x in range(min_x, max_x + 1):
-    #         print(
-    #             "#" if (x, y) in walls else ("E" if (x, y) in exterior else "."), end=""
-    #         )
+    #         if x == 0 and y == 0:
+    #             print("S", end="")
+    #         else:
+    #             print("#" if (x, y) in walls else ".", end="")
     #     print()
-    return (max_x - min_x + 1) * (max_y - min_y + 1) - len(exterior)
+    return area // 2 + 1
